6c1d735c16baa5f9dd060bb38e5c9bdd00499349
JorgeCaetano90 <112181487+JorgeCaetano90@users.noreply.github.com>
2024-06-27 20:40:59 -0300

    fornecedor

---
+++ go-crud-api/controllers/fornecedor_controller.go
@@ -0,0 +1,154 @@
+package controllers
+
+import (
+	"database/sql"
+	"encoding/json"
+	"go-crud-api/config"
+	"go-crud-api/models"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+func GetFornecedores(w http.ResponseWriter, r *http.Request) {
+	db, err := config.Connect()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+	rows, err := db.Query("SELECT idfornecedor, nome, cnpj, logradouro, numero, bairro, cep, telefone, idcidade FROM fornecedor")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+	var fornecedores []models.Fornecedor
+	for rows.Next() {
+		var fornecedor models.Fornecedor
+		if err := rows.Scan(&fornecedor.ID, &fornecedor.Nome, &fornecedor.Cnpj, &fornecedor.Logradouro, &fornecedor.Numero, &fornecedor.Bairro, &fornecedor.Cep, &fornecedor.Telefone, &fornecedor.IDCidade); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		fornecedores = append(fornecedores, fornecedor)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(fornecedores)
+}
+
+func GetFornecedor(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, "ID do fornecedor inválido", http.StatusBadRequest)
+		return
+	}
+
+	db, err := config.Connect()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	var fornecedor models.Fornecedor
+	err = db.QueryRow("SELECT idfornecedor, nome, cnpj, logradouro, numero, bairro, cep, telefone, idcidade FROM fornecedor WHERE idfornecedor = ?", id).Scan(&fornecedor.ID, &fornecedor.Nome, &fornecedor.Cnpj, &fornecedor.Logradouro, &fornecedor.Numero, &fornecedor.Bairro, &fornecedor.Cep, &fornecedor.Telefone, &fornecedor.IDCidade)
+	if err == sql.ErrNoRows {
+		http.Error(w, "Fornecedor não encontrado", http.StatusNotFound)
+		return
+	} else if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(fornecedor)
+}
+
+func CreateFornecedor(w http.ResponseWriter, r *http.Request) {
+	var fornecedor models.Fornecedor
+	if err := json.NewDecoder(r.Body).Decode(&fornecedor); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	db, err := config.Connect()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	result, err := db.Exec("INSERT INTO fornecedor (nome, cnpj, logradouro, numero, bairro, cep, telefone, idcidade) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", fornecedor.Nome, fornecedor.Cnpj, fornecedor.Logradouro, fornecedor.Numero, fornecedor.Bairro, fornecedor.Cep, fornecedor.Telefone)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fornecedor.ID = int(id)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(fornecedor)
+}
+
+func UpdateFornecedor(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, "ID fornecedor inválido", http.StatusBadRequest)
+		return
+	}
+
+	var fornecedor models.Fornecedor
+	if err := json.NewDecoder(r.Body).Decode(&fornecedor); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	db, err := config.Connect()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	_, err = db.Exec("UPDATE fornecedor SET nome = ?,  WHERE idfornecedor = ?", fornecedor.Nome, fornecedor.Cnpj, fornecedor.Logradouro, fornecedor.Numero, fornecedor.Bairro, fornecedor.Cep, fornecedor.Telefone, id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fornecedor.ID = id
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(fornecedor)
+}
+
+func DeleteFornecedor(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, "ID fornecedor inválido", http.StatusBadRequest)
+		return
+	}
+
+	db, err := config.Connect()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	_, err = db.Exec("DELETE FROM fornecedor WHERE idfornecedor = ?", id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
--- go-crud-api/routers/router.go
+++ go-crud-api/routers/router.go
@@ -18,14 +18,17 @@ func SetupRouter() *mux.Router {
 	router.HandleFunc("/usuario/{id}", controllers.UpdateUser).Methods("PUT")
 	router.HandleFunc("/usuario/{id}", controllers.DeleteUser).Methods("DELETE")
 
-
-	
 	router.HandleFunc("/venda", controllers.GetVendas).Methods("GET")
 	router.HandleFunc("/venda/{id}", controllers.GetVenda).Methods("GET")
 	router.HandleFunc("/venda", controllers.CreateVenda).Methods("POST")
 	router.HandleFunc("/venda/{id}", controllers.UpdateVenda).Methods("PUT")
 	router.HandleFunc("/venda/{id}", controllers.DeleteVenda).Methods("DELETE")
 
+	router.HandleFunc("/fornecedor", controllers.GetFornecedores).Methods("GET")
+	router.HandleFunc("/fornecedor/{id}", controllers.GetFornecedor).Methods("GET")
+	router.HandleFunc("/fornecedor", controllers.CreateFornecedor).Methods("POST")
+	router.HandleFunc("/fornecedor/{id}", controllers.UpdateFornecedor).Methods("PUT")
+	router.HandleFunc("/fornecedor/{id}", controllers.DeleteFornecedor).Methods("DELETE")
 
 	router.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNotFound)
